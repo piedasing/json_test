0a66dd691060cd645f65783330e2a4234e3f0346
piedasing <a0939732111@gmail.com>
2018-05-03 15:13:17 +0800

    test

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 $('.btn').on('click',function(){
   $.ajax({
     type: 'GET',
-    url: './json/test.json',
+    url: 'https://raw.githubusercontent.com/piedasing/json_test/master/json/test.json',
     success: function(data){
       console.log(data)
       var json_data = JSON.parse(data)
-      console.log(json_data)
     }
   })
 })
